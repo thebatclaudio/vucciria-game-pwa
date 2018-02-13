78a9b47319f84974f540992d67643bf9afbdd2cf
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-02-13 22:36:01 +0100

    Simulation of cards mixing

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -12,6 +12,7 @@ export class PlayComponent implements OnInit {
   players: User[] = [];
 
   cards;
+  remainingCards;
 
   newPlayer: User = {
     id: 0,
@@ -28,14 +29,73 @@ export class PlayComponent implements OnInit {
   currentCard;
 
   constructor() { 
-    this.cards = [
-      {
-        name: "Mossa",
-        info: "Inventa una mossa che ognuno dovrÃ  ripetere ogni volta che dovrÃ  bere. Se sono giÃ  presenti altre mosse devono essere ripetute tutte sia ora che quando si beve! Chi dimentica la mossa beve due volte, ma chi non sta attento e accusa qualcuno di non aver fatto la mossa quando invece Ã¨ stata fatta beve per calunnia!", slug: "mossa"
-      }, { name: "Bevi", info: "Non c'Ã¨ nulla da spiegare!", slug: "bevi" }, { name: "Bevi o offri", info: "Non c'Ã¨ nulla da spiegare", slug: "bevi-offri" }, { name: "I tre dell'ave maria", info: "Bevi tu e chi sta seduto alla tua destra e alla tua sinistra!", slug: "tre-ave-maria" }, { name: "Tu e to cumpari", info: "Invita qualcuno a bere con te", slug: "tu-to-cumpari" }, { name: "Jolly", info: "Che culo, hai trovato un jolly. Quando vuoi usalo per non bere o per andare in bagno, ma ricorda che Ã¨ monouso.", slug: "jolly" }, { name: "PipÃ¬", info: "Quando vuoi puoi andare in bagno.", slug: "pipi" }, { name: "Sfida", info: "Sfida qualcuno a pari o dispari, carta forbice e pietra o qualunque altra sfida. Chi perde beve!", slug: "sfida" }, { name: "7 Bum", info: 'Inizia a giocare a 7 Bum. Si inizia a contare da 1 ed ognuno deve dire il numero successivo, sostituendo ai multipli di 7 e ai numeri che finiscono con 7 la parola "bum". Chi sbaglia beve!', slug: "7-bum" }, { name: "21", info: "21 Ã¨ un gioco che consiste nel contare in gruppo fino a 21. Ognuno puÃ² dire uno o due numeri, ma quando vengono detti due numeri il giro inverte il suo senso. Chi Ã¨ costretto a dire 21 beve!", slug: "21" }, { name: "Storia", info: "Ognuno dice una parola ricordando in ordine tutte le parole dette precedentemente per formare una frase. Chi sbaglia la frase beve!", slug: "storia" }, {
-        name: "Zing Boing", info: 'Zing Boing Ã¨ un gioco di gruppo che consiste nel dire "zing" o "boing" in ordine orario o antiorario. Dicendo zing il giro continua normalmente, dicendo boing si inverte il senso del giro. Chi sbaglia beve!',
-        slug: "zing-boing"
-      }];
+    this.cards = [  
+      {  
+         name:"Mossa",
+         info:"Inventa una mossa che ognuno dovrà  ripetere ogni volta che dovrà  bere. Se sono già  presenti altre mosse devono essere ripetute tutte sia ora che quando si beve! Chi dimentica la mossa beve due volte, ma chi non sta attento e accusa qualcuno di non aver fatto la mossa quando invece è stata fatta beve per calunnia!",
+         slug:"mossa"
+      },
+      {  
+         name:"Bevi",
+         info:"Non c'è nulla da spiegare!",
+         slug:"bevi"
+      },
+      {  
+         name:"Bevi o offri",
+         info:"Non c'è nulla da spiegare",
+         slug:"bevi-offri"
+      },
+      {  
+         name:"I tre dell'ave maria",
+         info:"Bevi tu e chi sta seduto alla tua destra e alla tua sinistra!",
+         slug:"tre-ave-maria"
+      },
+      {  
+         name:"Tu e to cumpari",
+         info:"Invita qualcuno a bere con te",
+         slug:"tu-to-cumpari"
+      },
+      {  
+         name:"Jolly",
+         info:"Che culo, hai trovato un jolly. Quando vuoi usalo per non bere o per andare in bagno, ma ricorda che è monouso.",
+         slug:"jolly"
+      },
+      {  
+         name:"Pipì",
+         info:"Quando vuoi puoi andare in bagno.",
+         slug:"pipi"
+      },
+      {  
+         name:"Sfida",
+         info:"Sfida qualcuno a pari o dispari, carta forbice e pietra o qualunque altra sfida. Chi perde beve!",
+         slug:"sfida"
+      },
+      {  
+         name:"7 Bum",
+         info:'Inizia a giocare a 7 Bum. Si inizia a contare da 1 ed ognuno deve dire il numero successivo, sostituendo ai multipli di 7 e ai numeri che finiscono con 7 la parola "bum". Chi sbaglia beve!',
+         slug:"7-bum"
+      },
+      {  
+         name:"21",
+         info:"21 è un gioco che consiste nel contare in gruppo fino a 21. Ognuno puà² dire uno o due numeri, ma quando vengono detti due numeri il giro inverte il suo senso. Chi è costretto a dire 21 beve!",
+         slug:"21"
+      },
+      {  
+         name:"Storia",
+         info:"Ognuno dice una parola ricordando in ordine tutte le parole dette precedentemente per formare una frase. Chi sbaglia la frase beve!",
+         slug:"storia"
+      },
+      {  
+         name:"Zing Boing",
+         info:'Zing Boing è un gioco di gruppo che consiste nel dire "zing" o "boing" in ordine orario o antiorario. Dicendo zing il giro continua normalmente, dicendo boing si inverte il senso del giro. Chi sbaglia beve!',
+         slug:"zing-boing"
+      },
+      {  
+        name:"Bevono tutti",
+        info:'Non c\'è nulla da spiegare!',
+        slug:"bevono-tutti"
+      }
+   ];
   }
 
   ngOnInit() {
@@ -61,6 +121,7 @@ export class PlayComponent implements OnInit {
   }
 
   startGame() {
+    this.remainingCards = this.cards.slice();
     if(this.players.length >= 2) {
       this.nextCard();
       this.play = true;
@@ -68,14 +129,17 @@ export class PlayComponent implements OnInit {
   }
 
   nextCard() {
+    if(this.remainingCards.length == 0) {
+      this.remainingCards = this.cards.slice();
+    }
+
     if(this.currentPlayerIndex == this.players.length) {
       this.currentPlayerIndex = 0;
     }
 
-    console.log(this.cards);
-    this.currentCardIndex = Math.floor((Math.random() * this.cards.length));
-    console.log(this.currentCardIndex);
-    this.currentCard = this.cards[this.currentCardIndex];
+    this.currentCardIndex = Math.floor((Math.random() * this.remainingCards.length));
+    this.currentCard = this.remainingCards[this.currentCardIndex];
+    this.remainingCards.splice(this.currentCardIndex, 1);
 
     this.currentPlayer = this.players[this.currentPlayerIndex];
 
